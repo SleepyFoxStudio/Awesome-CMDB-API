2e2ab8a2f8967be02150360684847c2d8137a641
Mark Richardson <spiderbikejunk@hotmail.co.uk>
2022-03-26 21:33:58 +0000

    PoC of Meilisearch passed

--- src/Api/Controllers/AccountSummaryController.cs
+++ src/Api/Controllers/AccountSummaryController.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Awesome_CMDB_DataAccess_Models;
+using Meilisearch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Api.Controllers
 {
-    [Route("accountSummary")]
-    public class AccountSummaryController : ControllerBase
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
     {
+        private readonly ILogger _logger;
+
+        public AccountsController(ILogger<AccountsController> logger)
+        {
+            _logger = logger;
+        }
+
 
         /// <summary>
         /// Get the account summary listing accounts and username etc.
         /// </summary>
         /// <returns>AwesomeAccountSummary</returns>
         [HttpGet]
+        [ProducesResponseType(typeof(List<Account>), 200)]
         public IActionResult Get()
         {
             var accountId = User.GetAccountId();
@@ -23,13 +35,13 @@ namespace Api.Controllers
             {
                 new Account
                 {
-                    AccountId = "AWS-exampleAccountNumber-00001",
+                    Id = "AWS-exampleAccountNumber-00001",
                     AccountName = "My test AWS account",
                     DatacenterType = DatacenterType.AWS
                 },
                 new Account
                 {
-                    AccountId = "AWS-exampleAccountNumber-00002",
+                    Id = "AWS-exampleAccountNumber-00002",
                     AccountName = "My other test AWS account",
                     DatacenterType = DatacenterType.AWS
                 }
@@ -44,12 +56,23 @@ namespace Api.Controllers
         /// Sets the account summary listing accounts and username etc.
         /// </summary>
         [HttpPost]
-        public IActionResult Post(Account awesomeAccount)
+        public async Task<IActionResult> Post([FromBody]Account awesomeAccount)
         {
-            awesomeAccount.AccountId = User.GetAccountId();
-            //TODO save awesomeAccount somewhere
+            awesomeAccount.UserAccountId = User.GetAccountId();
+            _logger.LogInformation("Storing an account");
+            _logger.LogInformation($"{awesomeAccount.Id} - {awesomeAccount.AccountName}");
+            _logger.LogInformation($"Contains {awesomeAccount.ServerGroups.Count} Server groups, with a total of {awesomeAccount.ServerGroups.SelectMany(a=> a.Servers).Count()} servers");
 
 
+            var client = new MeilisearchClient("http://10.0.50.74:7700", "masterKey");
+
+            var index = client.Index("Accounts");
+            var documents = new List<Account> { awesomeAccount };
+            var task = await index.AddDocumentsAsync(documents);
+
+            var servers = awesomeAccount.ServerGroups.SelectMany(s => s.Servers);
+            await client.Index("Servers").AddDocumentsAsync(servers);
+            _logger.LogDebug(task.Status);
             return Ok();
         }
     }
